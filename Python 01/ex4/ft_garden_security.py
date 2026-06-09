d651da6f6dfb26daced91a20427eb51d497290b1
Lucas-pin <lucasfacundopin@gmail.com>
2026-06-09 21:07:46 +0200

    add/ex4 passign flake8 and mypy

--- Python 01/ex4/ft_garden_security.py
+++ Python 01/ex4/ft_garden_security.py
@@ -3,28 +3,21 @@ class Plant:
                  height: float = 0.0,
                  age: int = 0) -> None:
         self._init = True
-        self._height = 0
-        self._initial_height = 0
+        self._height = 0.0
+        self._initial_height = 0.0
         self._plant_age = 0
-        self._name = name
+        self.name = name
 
         try:
-            self._height = self.set_height(height)
-            self._initial_height = self.set_height(height)
-            self._plant_age = self.set_age(age)
+            self.set_height(height)
+            self.set_height(height)
+            self.set_age(age)
         except ValueError as error:
             print(f"{self.name}: {error}")
 
-    def grow(self, height: float) -> None:
-        self._height += self.set_height(height)
-        self.total_height = self.height - self.initial_height
-
-    def age(self, age: int = 1):
-        self._plant_age += age
-
     def show(self) -> None:
-        print(f"{self.name}: {round(self.height, 2)}cm,"
-              f" {self.plant_age} days old")
+        print(f"{self.name}: {round(self._height, 2)}cm,"
+              f" {self._plant_age} days old")
 
     def get_height(self) -> float:
         return self._height
@@ -32,9 +25,7 @@ class Plant:
     def set_height(self, height: float) -> None:
         if (height > 0 and height != self._height):
             self._height = height
-            print(f"Height updated: {self._height}cm")
         elif (height < 0):
-            print("Height update rejected")
             raise ValueError("Error, height can't be negative")
 
     def get_age(self) -> int:
@@ -43,7 +34,41 @@ class Plant:
     def set_age(self, age: int) -> None:
         if (age > 0 and self._plant_age != age):
             self._plant_age = age
-            print(f"Age updated: {self._height}days")
         elif (age < 0):
-            print("Age update rejected")
             raise ValueError("Error, age can't be negative")
+
+
+def main() -> None:
+    print("=== Garden Security System ===")
+
+    try:
+        rose: Plant = Plant("Rose", 15.0, 10)
+        print("Plant created: ", end='')
+        rose.show()
+    except ValueError as error:
+        print(f"Plant creation rejected: {error}")
+
+    try:
+        rose.set_height(25.0)
+        print(f"Height updated: {rose.get_height()}cm\n")
+        rose.set_height(-1.0)
+        print(f"Height updated: {rose.get_height()}cm\n")
+    except ValueError as error:
+        print(f"{rose.name.capitalize()}: {error}")
+        print("Height update rejected\n")
+
+    try:
+        rose.set_age(30)
+        print(f"Age updated: {rose.get_age()}days\n")
+        rose.set_age(-1)
+        print(f"Age updated: {rose.get_age()}days\n")
+    except ValueError as error:
+        print(f"{rose.name.capitalize()}: {error}")
+        print("Age update rejected\n")
+
+    print("Current state: ", end='')
+    rose.show()
+
+
+if __name__ == "__main__":
+    main()
