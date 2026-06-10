06464fd3ea1b167d61132caf41eb7297bfb6faab
Lucas-pin <lucasfacundopin@gmail.com>
2026-06-10 23:01:57 +0200

    refactor/delete unnecesary variables and try-except on constructor

--- Python 01/ex4/ft_garden_security.py
+++ Python 01/ex4/ft_garden_security.py
@@ -2,27 +2,19 @@ class Plant:
     def __init__(self, name: str = "default",
                  height: float = 0.0,
                  age: int = 0) -> None:
-        self._init = True
-        self._height = 0.0
-        self._plant_age = 0
         self.name = name
-
-        try:
-            self.set_height(height)
-            self.set_height(height)
-            self.set_age(age)
-        except ValueError as error:
-            print(f"{self.name}: {error}")
+        self.set_height(height)
+        self.set_age(age)
 
     def show(self) -> None:
         print(f"{self.name}: {round(self._height, 2)}cm,"
-              f" {self._plant_age} days old")
+              f"{self._plant_age} days old")
 
     def get_height(self) -> float:
         return self._height
 
     def set_height(self, height: float) -> None:
-        if (height > 0 and height != self._height):
+        if (height > 0):
             self._height = height
         elif (height < 0):
             raise ValueError("Error, height can't be negative")
@@ -31,7 +23,7 @@ class Plant:
         return self._plant_age
 
     def set_age(self, age: int) -> None:
-        if (age > 0 and self._plant_age != age):
+        if (age > 0):
             self._plant_age = age
         elif (age < 0):
             raise ValueError("Error, age can't be negative")
