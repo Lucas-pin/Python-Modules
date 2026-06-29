3150faa37db83782b24afe0a6496fcd8062ad697
Lucas-pin <lucasfacundopin@gmail.com>
2026-06-29 23:44:56 +0200

    feat/add ex3

---
+++ Python 03/ex3/ft_achievement_tracker.py
@@ -0,0 +1,43 @@
+import random
+
+ACHIEVEMENTS = ['Crafting Genius', 'Strategist', 'World Savior',
+                'Speed Runner', 'Survivor', 'Master Explorer',
+                'Treasure Hunter', 'Unstoppable', 'First Steps',
+                'Collector Supreme', 'Untouchable', 'Sharp Mind',
+                'Boss Slayer']
+
+
+def gen_player_achievements() -> set:
+    k = random.randint(1, len(ACHIEVEMENTS))
+    return set(random.choices(ACHIEVEMENTS, k=k))
+
+def main() -> None:
+    print("=== Achievement Tracker System ===\n")
+
+    alice = gen_player_achievements()
+    bob = gen_player_achievements()
+    charlie = gen_player_achievements()
+    dylan = gen_player_achievements()
+
+    print(f"Player Alice: {alice}")
+    print(f"Player Bob: {bob}")
+    print(f"Player Charlie: {charlie}")
+    print(f"Player Dylan: {dylan}")
+
+    print(f"\nAll distintct achievements: {set.union(alice, bob, charlie, dylan)}\n")
+
+    print(f"Common achievements: {set.intersection(alice, bob, charlie, dylan)}\n")
+
+    print(f"Only Alice has: {alice.difference(bob, charlie, dylan)}")
+    print(f"Only Bob has: {bob.difference(alice, charlie, dylan)}")
+    print(f"Only Charlie has: {charlie.difference(bob, alice, dylan)}")
+    print(f"Only Dylan has: {dylan.difference(bob, charlie, alice)}\n")
+
+    print(f"Alice is missing: {set(ACHIEVEMENTS).difference(alice)}")
+    print(f"Bob is missing: {set(ACHIEVEMENTS).difference(bob)}")
+    print(f"Charlie is missing: {set(ACHIEVEMENTS).difference(charlie)}")
+    print(f"Dylan is missing: {set(ACHIEVEMENTS).difference(dylan)}")
+
+
+if __name__ == "__main__":
+    main()
