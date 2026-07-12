367b5e738c1e47b2c4ca2ff743a2721423c7163d
Lucas-pin <lucasfacundopin@gmail.com>
2026-07-12 20:37:58 +0200

    feat/ex0 tested and passed with flake8 and mypy --strict

--- Python 05/ex0/data_processor.py
+++ Python 05/ex0/data_processor.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
+
 class DataProcessor(ABC):
-    def __init__(self):
-        self._internal_data = []
+    def __init__(self) -> None:
+        self._internal_data: list[str] = []
+        self._consumed_counter: int = -1
 
     @abstractmethod
     def validate(self, data: Any) -> bool:
@@ -14,18 +16,25 @@ class DataProcessor(ABC):
         pass
 
     def output(self) -> tuple[int, str]:
-        pass
+        if not self._internal_data:
+            raise IndexError("Ingest more data to be consumed.")
+        self._consumed_counter += 1
+        oldest_item = self._internal_data.pop(0)
+        return self._consumed_counter, oldest_item
 
 
 class NumericProcessor(DataProcessor):
 
     def validate(self, data: Any) -> bool:
-        
+
         def _validate_numbers(data: Any) -> bool:
             if type(data) in (int, float):
                 return True
+            return False
 
-        if type(data) is list:
+        if type(data) in (int, float):
+            return True
+        elif type(data) is list:
             if (len(data) == 0):
                 return False
             return all(_validate_numbers(_) for _ in data)
@@ -33,19 +42,13 @@ class NumericProcessor(DataProcessor):
             return False
 
     def ingest(self, data: int | float | list[int | float]) -> None:
-        try:
-            if type(data) in (int, float):
-                    self._internal_data.append(str(data))
-            elif type(data) is list:
-                self._internal_data.extend([str(value) for value in data])
-            else:
-                raise TypeError("Data must be int, float or list[int | float]")
-        except Exception as ex:
-            print("Data cannot be converted to a string")
-            raise ValueError("Ingest data failed") from ex
-        
-        
+        if not self.validate(data):
+            raise TypeError("Data must be int, float or list[int | float]")
 
+        if type(data) in (int, float):
+            self._internal_data.append(str(data))
+        elif type(data) is list:
+            self._internal_data.extend([str(value) for value in data])
 
 
 class TextProcessor(DataProcessor):
@@ -61,15 +64,13 @@ class TextProcessor(DataProcessor):
         return False
 
     def ingest(self, data: str | list[str]) -> None:
-        try:
-            if isinstance(data, str):
-                    self._internal_data.append(data)
-            elif isinstance(data, list):
-                self._internal_data.extend([str(value) for value in data])
-            else:
-                raise TypeError("Data must be str or list[str]")
-        except Exception as ex:
-            raise ValueError("Ingest data failed") from ex
+        if not self.validate(data):
+            raise TypeError("Data must be str or list[str]")
+
+        if isinstance(data, str):
+            self._internal_data.append(data)
+        elif isinstance(data, list):
+            self._internal_data.extend([str(value) for value in data])
 
 
 class LogProcessor(DataProcessor):
@@ -82,18 +83,78 @@ class LogProcessor(DataProcessor):
 
             if (len(data) == 0):
                 return False
-            return all(type(k) is str and type(v) is str for k, v in data.items())
+            return all(type(k) is str and type(v) is str
+                       for k, v in data.items())
 
         if type(data) not in (dict, list):
             return False
 
         if type(data) is dict:
             return _validate_dict(data)
-        
+
         else:
             if (len(data) == 0):
                 return False
             return all(_validate_dict(_) for _ in data)
-    
+
     def ingest(self, data: dict[str, str] | list[dict[str, str]]) -> None:
-        pass
+        if not self.validate(data):
+            raise TypeError("Data must be a dict of string key-value pairs")
+
+        if isinstance(data, list):
+            for value in data:
+                self._internal_data.extend([f"'{str(k)}': '{str(v)}'"
+                                            for k, v in value.items()])
+
+        elif (isinstance(data, dict)):
+            self._internal_data.extend([f"'{str(k)}': '{str(v)}'"
+                                        for k, v in data.items()])
+
+
+def test_behavior(instance: DataProcessor, data: list[Any]) -> None:
+    print(f"Data used to make testing: '{data}'")
+    print("\n---Testing validate method---")
+    for value in data:
+        print(f"Trying to validate input {value}: {instance.validate(value)}")
+
+    print("\n---Testing ingest method---")
+    for value in data:
+        try:
+            instance.ingest(value)
+        except Exception as ex:
+            print(f"Got exception with ingestion of {value}: {ex}")
+
+    print("\n---Testing output method---")
+    while True:
+        try:
+            index, value = instance.output()
+            print(f"Value {index}: {value}")
+        except IndexError as ex:
+            print(ex)
+            break
+
+
+def main() -> None:
+
+    numeric: NumericProcessor = NumericProcessor()
+    text: TextProcessor = TextProcessor()
+    log: LogProcessor = LogProcessor()
+    print("=== Code Nexus - Data Processor ===")
+
+    print("Testing Numeric Processor...\n")
+    test_behavior(numeric, [42, '42', 42.42, [1, 2, 3],
+                            [42.42, 123, 0, 'abc'], 'Hello'])
+    print("Testing Text Processor...\n")
+    test_behavior(text, [42, 'Hello', 'Nexus', 'World', ['List', 'Prove'],
+                         [{'dict': 'prove'}], [], {}])
+    print("Testing Log Processor...\n")
+    test_behavior(log, [[{'log_level': 'NOTICE',
+                          'log_message': 'Connection to server'},
+                        {'log_level': 'ERROR',
+                         'log_message': 'Unauthorized access!!'}],
+                        {'log_level': 'INFO'}, {}, {'log_level': 1},
+                        "prueba", 123])
+
+
+if __name__ == "__main__":
+    main()
