bf4b704a13e2fe3416291c0e1d45eebc74833a87
Lucas-pin <lucasfacundopin@gmail.com>
2026-07-21 21:19:39 +0200

    feat/ add files until Part III checked by flake8 and mypy

---
+++ Python 06/ft_alembic_2.py
@@ -0,0 +1,5 @@
+import alchemy.elements as elem
+
+print("=== Alembic 2 ===")
+print("Accessing alchemy/elements.py using 'import ...' structure")
+print(elem.create_earth())
---
+++ Python 06/ft_transmutation_0.py
@@ -0,0 +1,4 @@
+import alchemy.transmutation.recipes as recipes
+
+print("=== Transmutation 0 ===")
+print(f"Testing lead to gold: {recipes.lead_to_gold()}")
